a9aa90f0d6c5a7c56534a3c401cb4e30ef526edf
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-03-21 16:22:59 +0000

    nav - implemented state for arrows (now need to hook it up to sub menu opening when hovered/focused)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,80 +7,82 @@ import { MdOutlineKeyboardArrowUp } from 'react-icons/md';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 export default function Navbar() {
-  const [hoveredMenu, setHoveredMenu] = useState(false);
+  const [hoveredMenu, setHoveredMenu] = useState<number | null>(null);
 
-  const onMenuEnter = () => {
-    setHoveredMenu(true);
+  const onMenuEnter = (index: number) => {
+    setHoveredMenu(index);
   };
   const onMenuLeave = () => {
-    setHoveredMenu(false);
+    setHoveredMenu(null);
   };
 
   return (
-    <nav className="bg-white/95 md:sticky top-0 p-1 pb-2 border-b border-0.5">
-      <div className="flex items-center justify-between">
-        <a href="" className="flex items-center ml-2">
-          <img className="p-1" src="/logo.png" alt="logo" width={40} />
-          <h1 className="text-xl pl-1 text-black/80">Chester Mosque</h1>
-        </a>
-        <div className="">
-          <div className="hidden md:flex">
-            <ul className="flex gap-6">
-              <li onMouseEnter={onMenuEnter} onMouseLeave={onMenuLeave}>
-                <a className="hover:text-gray-600 flex items-center" href="/about">
-                  About
-                  {hoveredMenu ? (
-                    <MdOutlineKeyboardArrowUp className="ml-1" />
-                  ) : (
-                    <MdOutlineKeyboardArrowDown className="ml-1" />
-                  )}
-                </a>
-              </li>
-              <li>
-                <a className="hover:text-gray-600" href="/timetable">
-                  Timetable
-                </a>
-              </li>
-              <li>
-                <a className="hover:text-gray-600" href="/live_stream">
-                  Live Stream
-                </a>
-              </li>
-              <li onMouseEnter={onMenuEnter} onMouseLeave={onMenuLeave}>
-                <a className="hover:text-gray-600 flex items-center" href="/events">
-                  Events
-                  {hoveredMenu ? (
-                    <MdOutlineKeyboardArrowUp className="ml-1" />
-                  ) : (
-                    <MdOutlineKeyboardArrowDown className="ml-1" />
-                  )}
-                </a>
-              </li>
-              <li onMouseEnter={onMenuEnter} onMouseLeave={onMenuLeave}>
-                <a className="hover:text-gray-600 flex items-center" href="/services">
-                  Services
-                  {hoveredMenu ? (
-                    <MdOutlineKeyboardArrowUp className="ml-1" />
-                  ) : (
-                    <MdOutlineKeyboardArrowDown className="ml-1" />
-                  )}
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex md:hidden">
-            <RxHamburgerMenu size={30} />
-          </div>
-        </div>
-        <div className="hidden md:flex">
-          <a
-            className="bg-indigo-600 text-white px-2 py-1 rounded-full hover:bg-indigo-600/80 mr-4"
-            href="/contact"
-          >
-            Contact
+    <>
+      <nav className="bg-white/95 md:sticky top-0 p-1 pb-2 border-b border-0.5">
+        <div className="flex items-center justify-between">
+          <a href="" className="flex items-center ml-2">
+            <img className="p-1" src="/logo.png" alt="logo" width={40} />
+            <h1 className="text-xl pl-1 text-black/80">Chester Mosque</h1>
           </a>
+          <div className="">
+            <div className="hidden md:flex">
+              <ul className="flex gap-6">
+                <li onMouseEnter={() => onMenuEnter(0)} onMouseLeave={onMenuLeave}>
+                  <a className="hover:text-gray-600 flex items-center" href="/about">
+                    About
+                    {hoveredMenu === 0 ? (
+                      <MdOutlineKeyboardArrowUp className="ml-1" />
+                    ) : (
+                      <MdOutlineKeyboardArrowDown className="ml-1" />
+                    )}
+                  </a>
+                </li>
+                <li>
+                  <a className="hover:text-gray-600" href="/timetable">
+                    Timetable
+                  </a>
+                </li>
+                <li>
+                  <a className="hover:text-gray-600" href="/live_stream">
+                    Live Stream
+                  </a>
+                </li>
+                <li onMouseEnter={() => onMenuEnter(1)} onMouseLeave={onMenuLeave}>
+                  <a className="hover:text-gray-600 flex items-center" href="/events">
+                    Events
+                    {hoveredMenu === 1 ? (
+                      <MdOutlineKeyboardArrowUp className="ml-1" />
+                    ) : (
+                      <MdOutlineKeyboardArrowDown className="ml-1" />
+                    )}
+                  </a>
+                </li>
+                <li onMouseEnter={() => onMenuEnter(2)} onMouseLeave={onMenuLeave}>
+                  <a className="hover:text-gray-600 flex items-center" href="/services">
+                    Services
+                    {hoveredMenu === 2 ? (
+                      <MdOutlineKeyboardArrowUp className="ml-1" />
+                    ) : (
+                      <MdOutlineKeyboardArrowDown className="ml-1" />
+                    )}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex md:hidden">
+              <RxHamburgerMenu size={30} />
+            </div>
+          </div>
+          <div className="hidden md:flex">
+            <a
+              className="bg-indigo-600 text-white px-2 py-1 rounded-full hover:bg-indigo-600/80 mr-4"
+              href="/contact"
+            >
+              Contact
+            </a>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
