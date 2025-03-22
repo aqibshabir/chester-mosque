d5741949f22deaa405a60333b57a2c50ef411c2e
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-03-22 14:46:20 +0000

    nav - about sub menu (rough design)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="md:sticky top-0 p-2 border-b border-white border-0.5 bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg">
+      <nav className="md:sticky top-0 p-2 bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg">
         <div className="flex items-center justify-between">
           <a href="/" className="flex items-center ml-1">
             <img className="" src="/logo.png" alt="logo" width={40} />
@@ -54,11 +54,46 @@ export default function Navbar() {
                       <div
                         onMouseEnter={() => onMenuEnter(0)}
                         onMouseLeave={() => onMenuLeave(true)}
-                        className="absolute top-16 left-1/2 -translate-x-[40%] w-[500px] h-[250px] p-2 rounded-2xl border border-0.5 shadow-md bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg"
+                        className="absolute top-14 left-1/2 -translate-x-[40%] w-[500px] h-[260px] p-2 shadow-md bg-gradient-to-br from-white-70 to-white/50 backdrop-blur-lg"
                       >
                         <div className="flex h-full w-full">
-                          <div className="bg-slate-200 rounded-xl m-2 p-2 w-[200px]">About us</div>
-                          <div>New to Islam</div>
+                          <a
+                            href="/about"
+                            className="bg-white/90 hover:bg-white m-2 p-2 w-[200px] flex flex-col justify-center items-center gap-1 rounded-2xl"
+                          >
+                            <img src="/logo.png" alt="logo" width={50} />
+                            <p className="font-semibold">About The Mosque</p>
+                            <p className="text-sm text-gray-400 text-center">
+                              Learn key details on the mosque, our team and FAQs.
+                            </p>
+                          </a>
+                          <div className="flex flex-col h-full m-2.5 gap-3.5">
+                            <a
+                              href="/about/staff"
+                              className="hover:bg-black/5 rounded-sm w-[250px] p-1 flex flex-col"
+                            >
+                              <p className="font-semibold">Our Staff</p>
+                              <p className="text-sm text-gray-600">
+                                Information on our staff & volunteering oppertunities
+                              </p>
+                            </a>
+                            <a
+                              href="/about/new"
+                              className="hover:bg-black/5 rounded-sm w-[250px] p-1"
+                            >
+                              <p className="font-semibold">New To Islam</p>
+                              <p className="text-sm text-gray-600">
+                                Information and resources for our new brothers and sisters
+                              </p>
+                            </a>
+                            <a
+                              href="/about/visit"
+                              className="hover:bg-black/5 rounded-sm w-[250px] p-1"
+                            >
+                              <p className="font-semibold">Visiting the Mosque</p>
+                              <p className="text-sm text-gray-600">Plan your next visit</p>
+                            </a>
+                          </div>
                         </div>
                       </div>
                     </>
@@ -87,7 +122,7 @@ export default function Navbar() {
                     <div
                       onMouseEnter={() => onMenuEnter(1)}
                       onMouseLeave={() => onMenuLeave(true)}
-                      className="absolute top-16 left-1/2 -translate-x-[40%] w-[500px] h-[250px] p-2 rounded-2xl border border-0.5 shadow-md bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg"
+                      className="absolute top-14 left-1/2 -translate-x-[40%] w-[500px] h-[260px] p-2 shadow-md bg-gradient-to-br from-white-70 to-white/50 backdrop-blur-lg"
                     >
                       events sub menu
                     </div>
@@ -106,7 +141,7 @@ export default function Navbar() {
                     <div
                       onMouseEnter={() => onMenuEnter(2)}
                       onMouseLeave={() => onMenuLeave(true)}
-                      className="absolute top-16 left-1/2 -translate-x-[40%] w-[500px] h-[250px] p-2 rounded-2xl border border-0.5 shadow-md bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg"
+                      className="absolute top-14 left-1/2 -translate-x-[40%] w-[500px] h-[260px] p-2 shadow-md bg-gradient-to-br from-white-70 to-white/50 backdrop-blur-lg"
                     >
                       services sub menu
                     </div>
