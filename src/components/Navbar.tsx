659716ac32f42d24413d393bf94de032d6ec1852
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-03-22 09:05:28 +0000

    nav - implemented basic sub menu (using useRef)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { MdOutlineKeyboardArrowUp } from 'react-icons/md';
@@ -8,26 +8,37 @@ import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 export default function Navbar() {
   const [hoveredMenu, setHoveredMenu] = useState<number | null>(null);
+  const menuTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const onMenuEnter = (index: number) => {
+    if (menuTimeoutRef.current) {
+      clearTimeout(menuTimeoutRef.current);
+    }
     setHoveredMenu(index);
   };
-  const onMenuLeave = () => {
-    setHoveredMenu(null);
+
+  const onMenuLeave = (subMenu = false) => {
+    if (subMenu) {
+      setHoveredMenu(null);
+    } else {
+      menuTimeoutRef.current = setTimeout(() => {
+        setHoveredMenu(null);
+      }, 300);
+    }
   };
 
   return (
     <>
       <nav className="bg-white/95 md:sticky top-0 p-1 pb-2 border-b border-0.5">
         <div className="flex items-center justify-between">
-          <a href="" className="flex items-center ml-2">
-            <img className="p-1" src="/logo.png" alt="logo" width={40} />
+          <a href="/" className="flex items-center ml-2">
+            <img className="" src="/logo.png" alt="logo" width={30} />
             <h1 className="text-xl pl-1 text-black/80">Chester Mosque</h1>
           </a>
-          <div className="">
+          <div>
             <div className="hidden md:flex">
               <ul className="flex gap-6">
-                <li onMouseEnter={() => onMenuEnter(0)} onMouseLeave={onMenuLeave}>
+                <li onMouseEnter={() => onMenuEnter(0)} onMouseLeave={() => onMenuLeave()}>
                   <a className="hover:text-gray-600 flex items-center" href="/about">
                     About
                     {hoveredMenu === 0 ? (
@@ -37,9 +48,15 @@ export default function Navbar() {
                     )}
                   </a>
                   {hoveredMenu === 0 && (
-                    <div className="absolute top-16 left-20 right-20 p-20 bg-white rounded-2xl border border-0.5">
-                      about sub menu
-                    </div>
+                    <>
+                      <div
+                        onMouseEnter={() => onMenuEnter(0)}
+                        onMouseLeave={() => onMenuLeave(true)}
+                        className="absolute top-16 left-1/2 -translate-x-[40%] w-[500px] h-[250px] p-2 bg-white rounded-2xl border border-0.5"
+                      >
+                        about sub menu
+                      </div>
+                    </>
                   )}
                 </li>
                 <li>
@@ -52,7 +69,7 @@ export default function Navbar() {
                     Live Stream
                   </a>
                 </li>
-                <li onMouseEnter={() => onMenuEnter(1)} onMouseLeave={onMenuLeave}>
+                <li onMouseEnter={() => onMenuEnter(1)} onMouseLeave={() => onMenuLeave()}>
                   <a className="hover:text-gray-600 flex items-center" href="/events">
                     Events
                     {hoveredMenu === 1 ? (
@@ -62,12 +79,16 @@ export default function Navbar() {
                     )}
                   </a>
                   {hoveredMenu === 1 && (
-                    <div className="absolute top-16 left-20 right-20 p-20 bg-white rounded-2xl border border-0.5">
+                    <div
+                      onMouseEnter={() => onMenuEnter(1)}
+                      onMouseLeave={() => onMenuLeave(true)}
+                      className="absolute top-16 left-1/2 -translate-x-[40%] w-[500px] h-[250px] p-2 bg-white rounded-2xl border border-0.5"
+                    >
                       events sub menu
                     </div>
                   )}
                 </li>
-                <li onMouseEnter={() => onMenuEnter(2)} onMouseLeave={onMenuLeave}>
+                <li onMouseEnter={() => onMenuEnter(2)} onMouseLeave={() => onMenuLeave()}>
                   <a className="hover:text-gray-600 flex items-center" href="/services">
                     Services
                     {hoveredMenu === 2 ? (
@@ -77,7 +98,11 @@ export default function Navbar() {
                     )}
                   </a>
                   {hoveredMenu === 2 && (
-                    <div className="absolute top-16 left-20 right-20 p-20 bg-white rounded-2xl border border-0.5">
+                    <div
+                      onMouseEnter={() => onMenuEnter(2)}
+                      onMouseLeave={() => onMenuLeave(true)}
+                      className="absolute top-16 left-1/2 -translate-x-[40%] w-[500px] h-[250px] p-2 bg-white rounded-2xl border border-0.5"
+                    >
                       services sub menu
                     </div>
                   )}
