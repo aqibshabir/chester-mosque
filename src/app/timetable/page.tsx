7b415e1fd1ff809ce581e8c1270c4ca680f05c73
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-03-26 21:10:51 +0000

    timetable - added state to the select (needs to change to give string for start-end dates - will do next)

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import { getDates } from '@/lib/getDates';
 import {
   Select,
@@ -7,14 +9,16 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 
+const dates = getDates();
+
 export default function TimeTable() {
-  const dates = getDates();
+  const [selectedDate, setSelectedDate] = useState<string>(dates[0].label);
 
   return (
     <>
       <main className="bg-white">
         <div className="h-200 mt-2 p-4">
-          <Select defaultValue={dates[0].label}>
+          <Select defaultValue={selectedDate} onValueChange={setSelectedDate}>
             <SelectTrigger className="w-[180px]">
               <SelectValue />
             </SelectTrigger>
