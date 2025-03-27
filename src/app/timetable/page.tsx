87d072026b552aafb64726d8126cc258678af5fb
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-03-27 16:47:49 +0000

    timetable - intial timetable added (shadcn for table + useEffect/State for month's data)

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   Select,
@@ -8,6 +8,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 import { getMonthlyTimeTable } from '@/api/getMonthlyTimetable';
 import { getDates } from '@/lib/getDates';
@@ -19,10 +28,34 @@ interface MonthOption {
   label: string;
 }
 
+interface Timings {
+  Fajr: string;
+  Sunrise: string;
+  Dhuhr: string;
+  Asr: string;
+  Sunset: string;
+  Maghrib: string;
+  Isha: string;
+  Imsak: string;
+  Midnight: string;
+  Firstthird: string;
+  Lastthird: string;
+}
+
+interface DayData {
+  timings: Timings;
+  date: {
+    readable: string;
+    timestamp: string;
+  };
+  meta: unknown;
+}
+
 const dates = getDates();
 
 export default function TimeTable() {
   const [selectedDate, setSelectedDate] = useState<MonthOption>(dates[0]);
+  const [monthData, setMonthData] = useState<DayData[]>();
 
   const handleDateChange = (label: string) => {
     if (selectedDate.label !== label) {
@@ -32,12 +65,23 @@ export default function TimeTable() {
 
   const dateRanges = getStartAndEndDate(selectedDate.year, selectedDate.month);
 
-  // console.log(getMonthlyTimeTable(dateRanges[0], dateRanges[1]));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getMonthlyTimeTable(dateRanges[0], dateRanges[1]);
+        setMonthData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [selectedDate]);
 
   return (
     <>
       <main className="bg-white">
-        <div className="h-200 mt-2 p-4">
+        <div className="mt-2 p-4">
           <Select defaultValue={selectedDate.label} onValueChange={(e) => handleDateChange(e)}>
             <SelectTrigger className="w-[180px]">
               <SelectValue />
@@ -51,7 +95,44 @@ export default function TimeTable() {
             </SelectContent>
           </Select>
           <div>
-            {dateRanges[0]} {dateRanges[1]}
+            <Table>
+              <TableCaption>A timetable of prayer times.</TableCaption>
+              <TableHeader>
+                <TableRow className="">
+                  <TableHead className="text-left">Date</TableHead>
+                  <TableHead className="text-center">Fajr</TableHead>
+                  <TableHead className="text-center">Dhuhr</TableHead>
+                  <TableHead className="text-center">Asr</TableHead>
+                  <TableHead className="text-center">Maghreb</TableHead>
+                  <TableHead className="text-center">Isha</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {monthData?.map((day) => (
+                  <TableRow>
+                    <TableCell className="text-left" key={day.date.readable}>
+                      {day.date.readable}
+                    </TableCell>
+                    <TableCell className="text-center" key={day.timings.Fajr}>
+                      {day.timings.Fajr}
+                    </TableCell>
+                    <TableCell className="text-center" key={day.timings.Dhuhr}>
+                      {day.timings.Dhuhr}
+                    </TableCell>
+                    <TableCell className="text-center" key={day.timings.Asr}>
+                      {day.timings.Asr}
+                    </TableCell>
+                    <TableCell className="text-center" key={day.timings.Maghrib}>
+                      {day.timings.Maghrib}
+                    </TableCell>
+                    <TableCell className="text-center" key={day.timings.Isha}>
+                      {day.timings.Isha}
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow></TableRow>
+              </TableBody>
+            </Table>
           </div>
         </div>
       </main>
--- src/lib/getStartAndEndDate.ts
+++ src/lib/getStartAndEndDate.ts
@@ -1,6 +1,6 @@
 export const getStartAndEndDate = (year: number, month: number) => {
-  const startOfMonth = new Date(year, month, 1).toLocaleDateString().replaceAll('/', '-');
-  const endOfMonth = new Date(year, month + 1, 0).toLocaleDateString().replaceAll('/', '-');
+  const startOfMonth = new Date(year, month, 1).toLocaleDateString('en-GB').replaceAll('/', '-');
+  const endOfMonth = new Date(year, month + 1, 0).toLocaleDateString('en-GB').replaceAll('/', '-');
 
   return [startOfMonth, endOfMonth];
 };
