8267e728d9fdfb1e5728674fd456865690940f8f
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-04-30 17:39:35 +0100

    sanity studio - desk structure now handles singleton main pages (can't have multiple About/Events/Services pages) .. after I've tested sanity studio will begin to edit the schemas to handle other fields (and handle validation)

--- studio/structure/deskStructure.ts
+++ studio/structure/deskStructure.ts
@@ -1,6 +1,5 @@
-// /structure/deskStructure.ts
 import {StructureResolver} from 'sanity/structure'
-import {InfoOutlineIcon, CalendarIcon, CogIcon} from '@sanity/icons'
+import {InfoOutlineIcon, CalendarIcon, EarthGlobeIcon, DocumentTextIcon} from '@sanity/icons'
 
 export const deskStructure: StructureResolver = (S) =>
   S.list()
@@ -13,7 +12,10 @@ export const deskStructure: StructureResolver = (S) =>
           S.list()
             .title('About Pages')
             .items([
-              S.documentTypeListItem('aboutMainPageType').title('Main Page'),
+              S.listItem()
+                .title('Main Page')
+                .icon(DocumentTextIcon)
+                .child(S.document().schemaType('aboutMainPageType').documentId('aboutMainPage')),
               S.documentTypeListItem('aboutSubPageType').title('Sub Pages'),
             ]),
         ),
@@ -24,18 +26,30 @@ export const deskStructure: StructureResolver = (S) =>
           S.list()
             .title('Events Pages')
             .items([
-              S.documentTypeListItem('eventsMainPageType').title('Main Page'),
+              S.listItem()
+                .title('Main Page')
+                .icon(DocumentTextIcon)
+                .child(
+                  S.document().schemaType('eventsMainPageType').documentId('eventsMainPageType'),
+                ),
               S.documentTypeListItem('eventsSubPageType').title('Sub Pages'),
             ]),
         ),
       S.listItem()
         .title('Services')
-        .icon(CogIcon)
+        .icon(EarthGlobeIcon)
         .child(
           S.list()
             .title('Services Pages')
             .items([
-              S.documentTypeListItem('servicesMainPageType').title('Main Page'),
+              S.listItem()
+                .title('Main Page')
+                .icon(DocumentTextIcon)
+                .child(
+                  S.document()
+                    .schemaType('servicesMainPageType')
+                    .documentId('servicesMainPageType'),
+                ),
               S.documentTypeListItem('servicesSubPageType').title('Sub Pages'),
             ]),
         ),
