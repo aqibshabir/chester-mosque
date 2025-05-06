c2f78a21d93e6b5d4ab8dedbb1904c7e8b849002
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-05-06 16:57:18 +0100

    navbar - popups now give illusion of appearing behind navbar with shadow (subtle aesthetic treatment given to navbar)

--- src/components/Navbar/DesktopNav.tsx
+++ src/components/Navbar/DesktopNav.tsx
@@ -66,8 +66,12 @@ function DesktopNav({ about, events, services }: DesktopNavProps) {
                   }}
                   onMouseEnter={() => onMenuEnter(0)}
                   onMouseLeave={() => onMenuLeave(true)}
-                  className="absolute top-14 left-1/2 -translate-x-[46%] w-[500px] h-[260px] p-2 shadow-sm bg-gradient-to-br from-white-70 to-white/50 backdrop-blur-lg"
+                  className="absolute top-[54px] left-1/2 -translate-x-[46%] w-[500px] h-[260px] p-2 shadow-md bg-gradient-to-br from-white/60 to-white/50 backdrop-blur-lg"
                 >
+                  <div
+                    className="absolute top-0 left-0 w-full h-2 bg-gradient-to-b from-black/20 via-black/5 to-transparent"
+                    aria-hidden="true"
+                  />
                   <div className="flex h-full w-full">
                     <a
                       href="/about"
@@ -135,8 +139,12 @@ function DesktopNav({ about, events, services }: DesktopNavProps) {
                 }}
                 onMouseEnter={() => onMenuEnter(1)}
                 onMouseLeave={() => onMenuLeave(true)}
-                className="absolute top-14 left-1/2 -translate-x-[46%] w-[500px] h-[300px]  p-2 shadow-sm bg-gradient-to-br from-white-70 to-white/50 backdrop-blur-lg"
+                className="absolute top-[54px] left-1/2 -translate-x-[46%] w-[500px] h-[300px] p-2 shadow-md bg-gradient-to-br from-white/70 to-white/50 backdrop-blur-lg"
               >
+                <div
+                  className="absolute top-0 left-0 w-full h-2 bg-gradient-to-b from-black/20 via-black/5 to-transparent"
+                  aria-hidden="true"
+                />
                 <div className="flex h-full w-full justify-center items-center p-2">
                   <div className="grid grid-cols-2 gap-2 h-full">
                     {events.map((item) => (
@@ -177,8 +185,12 @@ function DesktopNav({ about, events, services }: DesktopNavProps) {
                 }}
                 onMouseEnter={() => onMenuEnter(2)}
                 onMouseLeave={() => onMenuLeave(true)}
-                className="absolute top-14 left-1/2 -translate-x-[46%] w-[500px] h-[220px] p-2 shadow-md bg-gradient-to-br from-white-70 to-white/50 backdrop-blur-lg"
+                className="absolute top-[54px] left-1/2 -translate-x-[46%] w-[500px] h-[220px] p-2 shadow-md bg-gradient-to-br from-white/70 to-white/50 backdrop-blur-lg"
               >
+                <div
+                  className="absolute top-0 left-0 w-full h-2 bg-gradient-to-b from-black/20 via-black/5 to-transparent"
+                  aria-hidden="true"
+                />
                 <div className="flex h-full w-full justify-center items-center p-2">
                   <div className="grid grid-cols-2 gap-2 h-full">
                     {services.map((item) => (
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -26,10 +26,10 @@ export default async function Navbar() {
     await sanityFetch<{ title: string; summary: string; slug: string }[]>(servicesQuery);
   return (
     <>
-      <nav className="sticky top-0 shadow-md p-2 bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg z-100">
+      <nav className="sticky top-0 shadow-md p-2 bg-gradient-to-br from-white/80 to-white/50 backdrop-blur-lg z-50">
         <div className="flex items-center justify-between">
           <div className="md:hidden" aria-hidden="true"></div>
-          <a href="/" className="flex items-center ml-1 z-50">
+          <a href="/" className="flex items-center ml-1">
             <img className="w-[50px] md:w-[38px]" src="/logo.png" alt="logo" />
             <h1 className="text-2xl md:text-lg lg:text-xl text-indigo-600/90 ml-3.5 md:ml-2.5 mr-1 flex flex-col leading-5.5 md:leading-4.5 font-medium">
               Chester Mosque
