7223a18887d1439b3ea595c39ddded8262f106c1
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-05-25 15:11:59 +0100

    all pages - hero sections heading changed size for mobile (apart from about)

--- src/app/events/components/hero.tsx
+++ src/app/events/components/hero.tsx
@@ -8,7 +8,7 @@ function Hero({ heading }: HeroProps) {
   return (
     <Ripple>
       <div className="relative flex flex-col gap-4 items-center justify-center px-4">
-        <h2 className="text-3xl sm:text-5xl md:text-7xl font-bold text-center">{heading}</h2>
+        <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold text-center">{heading}</h2>
         <p className="font-extralight text-base sm:text-xl md:text-4xl py-4 text-black/60">
           Where community comes together.
         </p>
