f13b3b544e84a7b58b39be18ce060c6b5e3254eb
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-05-26 15:50:14 +0100

    home - added simple hero section with images - will begin creating the ui for home page further now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,23 @@ export default function Home() {
   return (
     <>
       <main className="bg-white">
-        <div className="bg-slate-300 h-100 mt-16 m-6 rounded-2xl flex justify-center items-center"></div>
-        <div className="flex flex-col md:flex-row items-center justify-center m-2 mb-10">
-          <div className="bg-slate-300 h-80 w-80 m-6 rounded-2xl flex justify-center items-center"></div>
-          <div className="bg-slate-300 h-80 w-80 m-6 rounded-2xl flex justify-center items-center"></div>
-          <div className="bg-slate-300 h-80 w-80 m-6 rounded-2xl flex justify-center items-center"></div>
+        <img
+          className="absolute top-120 sm:top-90 xl:top-80 -left-0 sm:-left-10 xl:-left-0 w-40 sm:w-80 xl:w-100 z-10"
+          src="/flower-left.png"
+          alt="flower-left"
+        />
+        <img
+          className="absolute top-40 sm:top-100 -right-0 sm:-right-6 md:top-20 xl:-right-0 w-30 sm:w-60 xl:w-80 z-10"
+          src="/flower-right.png"
+          alt="flower-right"
+        />
+        <div className="relative m-6 rounded-2xl bg-slate-50 h-140 p-4">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h2 className="text-5xl sm:text-7xl md:text-8xl font-bold">Welcome</h2>
+            <p className="text-base sm:text-lg md:text-xl font-extralight">
+              To a Place of Peace and Prayer.
+            </p>
+          </div>
         </div>
       </main>
     </>
