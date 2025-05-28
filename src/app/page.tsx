93144834c8de24dfd272f62cee67f44660d381fb
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-05-28 18:24:48 +0100

    home - planning the ui for home page (added rough sections that I want to implenent - may change)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,39 @@
 export default function Home() {
   return (
     <>
-      <main className="bg-white">
+      <main className="">
         <img
-          className="absolute top-120 sm:top-90 xl:top-80 -left-0 sm:-left-10 xl:-left-0 w-40 sm:w-80 xl:w-100 z-10"
+          className="absolute top-110 sm:top-90 xl:top-80 -left-0 sm:-left-2 xl:-left-0 w-50 sm:w-80 xl:w-100 z-10"
           src="/flower-left.png"
           alt="flower-left"
         />
         <img
-          className="absolute top-40 sm:top-100 -right-0 sm:-right-6 md:top-20 xl:-right-0 w-30 sm:w-60 xl:w-80 z-10"
+          className="absolute top-30 sm:top-40 -right-0 sm:-right-6 md:top-20 md:-right-0 w-40 sm:w-60 xl:w-80 z-10"
           src="/flower-right.png"
           alt="flower-right"
         />
-        <div className="relative m-6 rounded-2xl bg-slate-50 h-140 p-4">
+        <div className="relative m-6 rounded-4xl bg-gray-50 h-140 p-4">
           <div className="flex flex-col justify-center items-center h-full">
             <h2 className="text-5xl sm:text-7xl md:text-8xl font-bold">Welcome</h2>
             <p className="text-base sm:text-lg md:text-xl font-extralight">
               To a Place of Peace and Prayer.
             </p>
           </div>
+        </div>
+        <div className="h-100 w-full bg-gray-200 my-20">
+          <p>Timetable</p>
+        </div>
+        <div className="h-100 w-full bg-gray-200 my-20">
+          <p>News/updates - sanity integration</p>
+        </div>
+        <div className="h-100 w-full bg-gray-200 my-20">
+          <p>Upcoming Events - sanity integration</p>
+        </div>
+        <div className="h-100 w-full bg-gray-200 my-20">
+          <p>Services we offer</p>
+        </div>
+        <div className="h-100 w-full bg-gray-200 my-20">
+          <p>Donate CTA</p>
         </div>
       </main>
     </>
