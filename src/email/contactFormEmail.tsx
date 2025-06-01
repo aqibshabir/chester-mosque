867845dd368af2836b4f54094936537327e5c67d
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-06-01 08:30:51 +0100

    contact - logo with text added to contactFormEmail

--- src/email/contactFormEmail.tsx
+++ src/email/contactFormEmail.tsx
@@ -45,52 +45,14 @@ export default function ContactFormEmail({
         <Body className="bg-gray-100 text-black">
           <Container>
             <Section className="bg-white border border-black my-10 px-10 py-4 rounded-xl">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                style={{ borderCollapse: 'collapse' }}
+              <Img
+                src="https://raw.githubusercontent.com/aqibshabir/chester-mosque/master/public/logo-with-text.png"
+                alt="logo"
+                width={192}
+                height={48}
+                style={{ display: 'block' }}
                 className="my-6"
-              >
-                <tbody>
-                  <tr>
-                    <td width="50" style={{ verticalAlign: 'middle' }}>
-                      <Img
-                        src="https://raw.githubusercontent.com/aqibshabir/chester-mosque/master/public/logo.png"
-                        alt="logo"
-                        width={50}
-                        height={50}
-                        style={{ display: 'block', maxWidth: '50px' }}
-                      />
-                    </td>
-                    <td style={{ paddingLeft: '8px', verticalAlign: 'middle' }}>
-                      <Text
-                        style={{
-                          margin: 0,
-                          fontSize: '24px',
-                          color: 'rgba(79, 70, 229, 0.9)',
-                          fontWeight: 500,
-                          lineHeight: 1.2,
-                        }}
-                      >
-                        Chester Mosque
-                      </Text>
-                      <Text
-                        style={{
-                          margin: 0,
-                          fontSize: '24px',
-                          color: 'rgba(0, 0, 0, 0.38)',
-                          fontWeight: 100,
-                          lineHeight: 1.1,
-                        }}
-                      >
-                        &amp; Islamic Centre
-                      </Text>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+              />
               <Heading className="leading-tight text-lg">
                 You recieved the following message from {name}
               </Heading>
